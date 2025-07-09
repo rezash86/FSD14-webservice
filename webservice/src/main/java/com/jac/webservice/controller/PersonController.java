4b65e7f11704bee17878f110192304c3611fd7a2
rezash86 <reza.sh@gmail.com>
2025-07-09 17:00:08 -0400

    path variable

--- webservice/src/main/java/com/jac/webservice/controller/PersonController.java
+++ webservice/src/main/java/com/jac/webservice/controller/PersonController.java
@@ -1,22 +1,31 @@
 package com.jac.webservice.controller;
 
 import com.jac.webservice.dto.Person;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/person")
 public class PersonController {
 
-    @GetMapping
-    public List<Person> getAll(){
-        return List.of
-                (
-                        Person.builder().id(1).name("A").build(),
-                        Person.builder().id(2).name("B").build()
-                );
+    List<Person> people = List.of
+            (
+                    Person.builder().id(1).name("A").build(),
+                    Person.builder().id(2).name("B").build()
+            );
+
+
+    @GetMapping("/")
+    public List<Person> getAll() {
+        return people;
+    }
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable int id) {
+        Optional<Person> optionalPerson = people.stream()
+                .filter(person -> person.getId() == id).findFirst();
+        return optionalPerson.orElse(null);
     }
 }
