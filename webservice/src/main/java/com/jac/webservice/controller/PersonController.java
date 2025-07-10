c24e8746873215efb696ebd90304c9a3a6f75ec5
rezash86 <reza.sh@gmail.com>
2025-07-09 20:12:34 -0400

    Delete

--- webservice/src/main/java/com/jac/webservice/controller/PersonController.java
+++ webservice/src/main/java/com/jac/webservice/controller/PersonController.java
@@ -32,8 +32,7 @@ public class PersonController {
 
     @GetMapping("/{id}")
     public Person getPerson(@PathVariable int id) {
-        Optional<Person> optionalPerson = people.stream()
-                .filter(person -> person.getId() == id).findFirst();
+        Optional<Person> optionalPerson = findById(id);;
         return optionalPerson.orElse(null);
     }
 
@@ -53,11 +52,25 @@ public class PersonController {
 
     @PutMapping("/{id}")
     public Person modifyPerson(@PathVariable int id, @RequestBody Person updatePerson){
-        Optional<Person> foundPerson = people.stream().filter(p -> p.getId() == id).findFirst();
+        Optional<Person> foundPerson = findById(id);
         if(foundPerson.isPresent()){
             foundPerson.get().setName(updatePerson.getName());
             return foundPerson.get();
         }
         return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean remove(@PathVariable int id){
+        Optional<Person> foundPerson = findById(id);
+        if(foundPerson.isPresent()){
+            people.remove(foundPerson.get());
+            return true;
+        }
+        return false;
+    }
+
+    private Optional<Person> findById(int id){
+        return people.stream().filter(p -> p.getId() == id).findFirst();
     }
 }
